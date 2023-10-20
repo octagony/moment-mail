9e25c434efd05496647c94a5bd63a2b32618bc12
octagony <moctagony@gmail.com>
2023-10-20 19:07:56 +0300

    [FEAT] Add shadcn/ui

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+     <>
+          <h1>Hello from Next.js 13</h1>
+          <Button>
+               Hello
+          </Button>
+     </>
+  )
+}
