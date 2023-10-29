5217d85ea0cf00024e1513d8cdf4dd94928855c9
octagony <moctagony@gmail.com>
2023-10-29 11:44:01 +0300

    [FEAT] Added lucide icons, theme toggler and theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,33 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "MomentMail",
-	description: "One Email? One moment!",
+  title: "MomentMail",
+  description: "One Email? One moment!",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<Navbar />
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
