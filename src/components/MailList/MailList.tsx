8efa1530adeef8bc36e00780069a6713cf5ce672
octagony <moctagony@gmail.com>
2023-12-31 20:50:19 +0300

    [FEAT] Added Alert component

--- src/components/MailList/MailList.tsx
+++ src/components/MailList/MailList.tsx
@@ -11,23 +11,51 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Input } from "@/components/ui/input";
 import { MoveRight, Trash2 } from "lucide-react";
 import useMails from "@/hooks/useMails";
 import { useMailStore } from "@/store/mailStore";
 import Link from "next/link";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 export default function MailList() {
   const [mail, domains] = useMailStore((store) => [
     store.email,
     store.domainInfo,
   ]);
+
+  const [mailList, setMailList] = useState<IMail[]>([]);
+
   const { mails } = useMails<IMail[]>(domains?.at(0)!, domains?.at(1)!);
+
+  useEffect(() => {
+    if (mails) {
+      setMailList(mails);
+    }
+  });
+
+  const handleDeleteMail = (deleteMailId: number) => {
+    const updatedMails = mailList?.filter((mail) => mail.id !== deleteMailId);
+    console.log(updatedMails);
+    //setMailList(mails);
+  };
+
   return (
     <div className="mt-12 flex flex-col gap-8">
       <div className="flex flex-row flex-wrap gap-4 justify-center align-center">
         {/*TODO: Add spinner, change layout*/}
-        {mails?.map((mail) => (
+        {mailList?.map((mail) => (
           <Card className="w-1/4">
             <CardHeader>
               <CardTitle>{mail.subject}</CardTitle>
@@ -36,11 +64,36 @@ export default function MailList() {
               </CardDescription>
             </CardHeader>
             <CardFooter className="flex justify-between">
-              {/*TODO: Implement delete mail logic*/}
-              <Button variant="outline">
-                <Trash2 className="mr-2" />
-                Delete
-              </Button>
+              <AlertDialog>
+                <AlertDialogTrigger>
+                  <Button
+                    variant="outline"
+                    onClick={() => handleDeleteMail(mail.id)}
+                  >
+                    <Trash2 className="mr-2" />
+                    Delete
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      this email
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => handleDeleteMail(mail.id)}
+                    >
+                      Delete
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
               <Link href={`/mail/${mail.id}`}>
                 <Button>
                   Open
--- src/store/mailStore.ts
+++ src/store/mailStore.ts
@@ -4,6 +4,7 @@ import { create } from "zustand";
 export const useMailStore = create<IEMail>((set) => ({
   email: "",
   domainInfo: [],
+  mailList: [],
   setEmail: (mail: string) => set((state) => ({ email: mail })),
   setDomainInfo: (domains: string[]) =>
     set((state) => ({ domainInfo: domains })),
